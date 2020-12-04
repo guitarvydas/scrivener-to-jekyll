ebe0a255f5ce1c64deb608db6800ffc091a44337
Paul Tarvydas <guitarvydas@gmail.com>
2020-12-04 09:41:40 -0500

    wire-splitter2 inputs from fd3 and outputs to fd4 and fd5

--- test-wire-splitter2.bash
+++ test-wire-splitter2.bash
@@ -5,11 +5,19 @@ fifo4=fifo_$RANDOM
 fifo5=fifo_$RANDOM
 mkfifo ${fifo3} ${fifo4} ${fifo5}
 
-./wire-splitter2 ${fifo3} ${fifo4} ${fifo5} &
+./wire-splitter2 3<${fifo3} 4>${fifo4} 5>${fifo5}&
 pid=$!
 
-echo hello testing testing >${fifo3}
-cat - <${fifo4}
-cat - <${fifo5}
+echo data-on-port-3 >${fifo3}
+
+# grrr, each fifo must have 2 active ends, or blocking will occur
+#  (the fifos are buffered, but only after both ends have been established)
+# hence, above call to wire-splitter2 will block until something attaches to fifo5
+# hence, we must & the port 4 line
+echo "port 4:" /`cat - <${fifo4}`/ &
+echo "port 5:" /`cat - <${fifo5}`/
 
 wait $pid
+
+echo "< test-wire-splitter2 done"
+
