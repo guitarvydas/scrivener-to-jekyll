8736b7a37f920b65f32a6fbdf23b2b280a0300f0
Paul Tarvydas <guitarvydas@gmail.com>
2020-12-04 08:18:36 -0500

    wire-splitter2 & test-wire-splitter2

--- test-wire-splitter2.bash
+++ test-wire-splitter2.bash
@@ -3,13 +3,15 @@
 fifo3=fifo_$RANDOM
 fifo4=fifo_$RANDOM
 fifo5=fifo_$RANDOM
-mkfifo $fifo3 $fifo4 $fifo5
+mkfifo ${fifo3}
+mkfifo ${fifo4}
+mkfifo ${fifo5}
 
-./wire-splitter2 $fifo3 $fifo4 $fifo5 &
+./wire-splitter2 ${fifo3} ${fifo4} ${fifo5} &
 pid=$!
 
-echo hello >$fifo3
-cat <$fifo4
-cat <$fifo5
+echo hello testing testing >${fifo3}
+cat - <${fifo4}
+cat - <${fifo5}
 
 wait $pid
