7dda03a4336ee7bffc83cc8d4ee8683bc04a8ef5
Paul Tarvydas <guitarvydas@gmail.com>
2020-12-05 15:10:10 -0500

    restructure
    wip: move-to-posts
    
    
    clean updated for new structure
    
    
    makenew script added
    
    
    create-prefix updated
    
    
    added debug-entry and debug-exit
    
    
    Test Engineering details
    
    
    pathA
    
    
    clean updated to kill all bash scripts from this login
    
    
    isAdirectory
    
    
    moved to _bin

--- pathB.bash
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-echo ">>>> pathB"
-read -u 3 junk
-read -u 4 junk
-read -u 5 junk
-echo "<<<< pathB"
--- test-isADirectory.bash
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-read -u 3 PORT_filename
-if test -d ${PORT_filename}
-then
-   echo ${PORT_filename} is a directory
-else
-    echo ${PORT_filename} is NOT a directory
-fi
-
